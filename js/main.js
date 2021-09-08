b589de7509d0be7a57ee26a4d7ec40de6ca0454f
Elton Souza <elton.souza1515@gmail.com>
2021-09-08 09:58:25 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -5,3 +5,9 @@ window.addEventListener('scroll', ()=>{
         document.querySelector('.cabecalho').classList.remove('cabecalho-azul')
     }
 })
+
+document.querySelectorAll('.nav-bar__itens').forEach(lista=>{
+    lista.addEventListener('click', ()=>{
+        document.querySelector('.btn-hamb').checked = false;
+    })
+})
