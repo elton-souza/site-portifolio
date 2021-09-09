79f94500a52b88cedd6eb944543e26581494bec3
Elton Souza <elton.souza1515@gmail.com>
2021-09-09 12:02:40 -0300

    contato

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ document.querySelectorAll('.nav-bar__itens').forEach(lista=>{
         document.querySelector('.btn-hamb').checked = false;
     })
 })
+
+document.querySelector('[data-email]').addEventListener('click', (btn)=>{
+    const form = document.querySelector('.bloco-forms')
+    if(btn.target.dataset.email == 0){
+        form.classList.add('visivel')
+        btn.target.dataset.email = 1
+    }else{
+        form.classList.remove('visivel')
+        btn.target.dataset.email = 0
+    }
+})
+
+document.querySelector('[data-wpp]').addEventListener('click', ()=>{
+    window.location.href = 'https://api.whatsapp.com/send?phone=+5596981337363&text=Olá,+estou entrando em contato através do seu portifólio'
+})
